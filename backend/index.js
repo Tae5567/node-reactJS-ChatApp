3bc0b5422a9b5a55752501a60ab6b61a544814a2
Eyitayo Ayodeji <79061412+Tae5567@users.noreply.github.com>
2023-04-22 22:51:29 +0100

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const axios = require("axios");
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "ec3ec7b0-ec68-470c-adcb-5c6d45514b23" } }
+
+    );
+    return res.status(r.status).json(r.data)
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data)
+  }
+});
+
+app.listen(3001);
